7500366222a93025a39d48b3db81d759e56838e2
MRprgmr <algoprogram01@gmail.com>
2021-08-16 17:49:43 +0500

    Set user ads status when he or she registered as a simple user

--- handlers/users/client/register_user.py
+++ handlers/users/client/register_user.py
@@ -95,7 +95,7 @@ async def confirm_user_information(call: CallbackQuery, state: FSMContext):
         user.Is_registered = True
         user.Is_Driver = False
         user.Age = None
-        user_ads = await sync_to_async(user.Saved_ads.all)()
+        user_ads = await sync_to_async(user.Saved_Ads.all)()
         for ads in user_ads:
             ads.status = False
             await sync_to_async(ads.save)()

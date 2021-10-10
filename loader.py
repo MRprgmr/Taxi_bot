99ce7167ad2a7ef685a87ef1bc04c7ce8651f2f2
--global <algoprogram01@gmail.com>
2021-10-10 09:23:50 -0700

    Ready to deploy

--- loader.py
+++ loader.py
@@ -1,8 +1,12 @@
 from aiogram import Bot, Dispatcher, types
+from aiogram.bot import api
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.bot.api import TelegramAPIServer
 
 from data import config
 
-bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
+api_server = TelegramAPIServer.from_base(config.SERVER)
+
+bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML, server=api_server)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
